23e99d8e6c7834d7974831ad4ebec344732fe0fb
Ariel <143091143+arielwongch@users.noreply.github.com>
2026-02-23 00:27:40 -0500

    update logo and some minor problem in current timetable ui

--- app/src/main/java/com/example/graphicaltimeplanner/Course.kt
+++ app/src/main/java/com/example/graphicaltimeplanner/Course.kt
@@ -1,5 +1,10 @@
 package com.example.graphicaltimeplanner
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+
 data class Section(
     val day: String,
     val startHour: Int,
