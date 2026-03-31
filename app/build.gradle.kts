487c84cf98041c13e6e1a7ac1568e6860b1bed7b
Wahab Khan <wahabk1234@gmail.com>
2026-03-31 17:35:28 -0400

    added messaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,6 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.messaging)
     implementation(libs.kotlinx.coroutines.play.services)
 }
